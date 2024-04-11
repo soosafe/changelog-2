639e44be9ea335d16dc7cf2df9cdb89301a4c1c1
karyanayandi <halo@yandi.me>
2024-04-11 15:07:30 +0700

    fix contributor

--- changelog/components/contributor.tsx
+++ changelog/components/contributor.tsx
@@ -5,14 +5,18 @@ interface ContributorProps {
     url: string;
   };
   name: string;
+  position: string;
 }
 
 export function Contributor(props: ContributorProps) {
   return (
     <HStack spacing={4}>
-      {!!props.avatar.url && <Avatar src={props.avatar.url} />}
+      {!!props.avatar.url && (
+        <Avatar src={`${process.env.NEXT_PUBLIC_PAYLOAD_URL}${props.avatar.url}`} />
+      )}
       <Flex direction="column" align="flex-start" justify="center">
         <Text fontWeight="semibold">{props.name}</Text>
+        <Text color="rgba(36,31,71,0.8)">{props.position}</Text>
       </Flex>
     </HStack>
   );
