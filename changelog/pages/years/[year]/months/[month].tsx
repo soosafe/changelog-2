f5e296b7a9d7422622f88895cc4ab4c70bf0ea3e
Karyana Yandi <xkaryanayandi@gmail.com>
2024-04-20 11:47:36 +0700

    change months page to server side

--- changelog/pages/years/[year]/months/[month].tsx
+++ changelog/pages/years/[year]/months/[month].tsx
@@ -54,31 +54,31 @@ const Page = ({ changelogs }: IPageProps) => {
   );
 };
 
-export async function getStaticPaths() {
-  const currentYear = new Date().getFullYear();
-  const years = Array.from(Array(41), (x, i) => currentYear - 20 + i); // Generate paths for 20 years back and forth from the current year
-  const months = Array.from(Array(12)).map((month, index) =>
-    index < 9 ? `0${index + 1}` : `${index + 1}`
-  );
+// export async function getStaticPaths() {
+//   const currentYear = new Date().getFullYear();
+//   const years = Array.from(Array(41), (x, i) => currentYear - 20 + i); // Generate paths for 20 years back and forth from the current year
+//   const months = Array.from(Array(12)).map((month, index) =>
+//     index < 9 ? `0${index + 1}` : `${index + 1}`
+//   );
+//
+//   const paths = months
+//     .map((month) =>
+//       years.map((year) => ({
+//         params: {
+//           month: month.toString(),
+//           year: year.toString(),
+//         },
+//       }))
+//     )
+//     .flat();
+//
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-  const paths = months
-    .map((month) =>
-      years.map((year) => ({
-        params: {
-          month: month.toString(),
-          year: year.toString(),
-        },
-      }))
-    )
-    .flat();
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export const getStaticProps = async ({ params }) => {
+export const getServerSideProps = async ({ params }) => {
   const changelogs = await api.get("/api/changelogs");
 
   const { month, year } = params;
